9e56e3f7aca635a5073f4519b4ad3b9e4d71ef64
ram4ik <ram4ik@hotmail.com>
2023-11-14 08:22:29 +0200

    The problem with a simple NavigationLink

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        Text("Detail View \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<1000) { i in
+                NavigationLink("Tap Me") {
+                    DetailView(number: i)
+                }
+            }
         }
-        .padding()
     }
 }
 
