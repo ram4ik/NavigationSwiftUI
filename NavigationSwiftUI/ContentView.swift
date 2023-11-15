d0c2daa11c7d1858547f9c90ac355b415f48d77b
ram4ik <ram4ik@hotmail.com>
2023-11-15 18:51:23 +0200

    Placing toolbar buttons in exact locations

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -10,14 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) { i in
-                Text("Row \(i)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue)
-            .toolbarColorScheme(.dark)
-            .toolbar(.hidden, for: .navigationBar)
+            Text("Hi")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Tap Me") {
+                            
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden()
         }
     }
 }
