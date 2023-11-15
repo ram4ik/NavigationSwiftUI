78aa9f35b7e758c7d44e6158de5abee7471fc01d
ram4ik <ram4ik@hotmail.com>
2023-11-15 15:06:40 +0200

    How to save NavigationStack paths using Codable

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -7,29 +7,56 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavePath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
 struct DetailsView: View {
     var number: Int
-    @Binding var path: [Int]
     
     var body: some View {
         NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path.removeAll()
-                }
-            }
     }
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailsView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            DetailsView(number: 0)
                 .navigationDestination(for: Int.self) { i in
-                    DetailsView(number: i, path: $path)
+                    DetailsView(number: i)
                 }
         }
     }
